9b0c2c5c62a372dc50210123c2cb51c6b4cacfdb
YY <z3388638@gmail.com>
2016-09-04 05:28:31 +0800

    add <AccountSetting />

---
+++ src/component/accountSetting.jsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Glyphicon from 'react-bootstrap/lib/Glyphicon';
+import Modal from 'react-bootstrap/lib/Modal';
+import Form from 'react-bootstrap/lib/Form';
+import FormControl from 'react-bootstrap/lib/FormControl';
+import FormGroup from 'react-bootstrap/lib/FormGroup';
+import Col from 'react-bootstrap/lib/Col';
+import Panel from 'react-bootstrap/lib/Panel';
+import Button from 'react-bootstrap/lib/Button';
+
+export default class AccountSetting extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			modalOpened: false
+		};
+		this.openModal = this.openModal.bind(this);
+		this.closeModal = this.closeModal.bind(this);
+		this.handleClick = this.handleClick.bind(this);
+	}
+	openModal() {
+		$.ajax({
+			url: '../../static/json/getOrderList.json', 
+			type: 'get', 
+			dataType: 'json',
+			success: (data) => {
+				this.setState({
+					modalOpened: true
+				}, () => {
+					ReactDOM.findDOMNode(this.connectTo).value = data.list[0] == undefined ? '' : data.list[0].id;
+				});
+			}, 
+			error: (jqXHR) => {
+				console.log(jqXHR);
+			}
+		})
+	}
+	closeModal() {
+		this.setState({
+			modalOpened: false
+		});
+	}
+	handleClick() {
+		alert(ReactDOM.findDOMNode(this.connectTo).value);
+	}
+	render() {
+		return (
+			<div id="btn-setting" onClick={this.openModal}>
+				<Glyphicon glyph="cog" onClick={this.openModal} />
+				<Modal id="modal-accountSetting" show={this.state.modalOpened} onHide={this.closeModal}>
+					<Modal.Header closeButton>
+						<Modal.Title>設定</Modal.Title>
+					</Modal.Header>
+					<Modal.Body>
+						<Panel header="轉接設定">
+							<Form horizontal>
+								<FormGroup controlId="connectTo">
+									<Col sm={3}>
+										帳號
+									</Col>
+									<Col sm={9}>
+										<FormControl 
+											type="text" 
+											placeholder="轉接帳號" 
+											ref={ref => this.connectTo = ref}
+										/>
+									</Col>
+								</FormGroup>
+							</Form>
+						</Panel>
+						<Button className="btn-saveSetting" bsStyle="primary" style={{width: '100%'}} onClick={this.handleClick}>完成</Button>
+					</Modal.Body>
+				</Modal>
+			</div>
+		);
+	}
+}
--- src/component/navigation.jsx
+++ src/component/navigation.jsx
@@ -3,6 +3,8 @@ import Navbar from 'react-bootstrap/lib/Navbar';
 import Nav from 'react-bootstrap/lib/Nav';
 import NavItem from 'react-bootstrap/lib/NavItem';
 
+import AccountSetting from './accountSetting';
+
 export default class Navigation extends React.Component {
 	constructor(props) {
 		super(props);
@@ -18,6 +20,9 @@ export default class Navigation extends React.Component {
 				<Navbar.Collapse>
 					<Nav pullRight>
 						<NavItem eventKey={1} href="#">{this.props.user}</NavItem>
+						<NavItem eventKey={1} href="#">
+							<AccountSetting user={this.props.user} />
+						</NavItem>
 						<NavItem eventKey={2} href="#" onClick={this.props.onLogout}>登出</NavItem>
 					</Nav>
 				</Navbar.Collapse>
